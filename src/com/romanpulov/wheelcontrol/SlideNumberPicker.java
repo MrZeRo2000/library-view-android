72fb759d34eef41314993ae4768a23dcc6a18afe
MrZeRo2000 <MrZeRo2000@outlook.com>
2015-05-08 18:59:59 +0300

    2015_05_08 EOD1

--- src/com/romanpulov/wheelcontrol/SlideNumberPicker.java
+++ src/com/romanpulov/wheelcontrol/SlideNumberPicker.java
@@ -14,6 +14,7 @@ import android.view.GestureDetector;
 import android.widget.OverScroller;
 import android.widget.Scroller;
 import android.widget.Toast;
+import android.widget.NumberPicker;
 
 public class SlideNumberPicker extends View implements GestureDetector.OnGestureListener {
 	
