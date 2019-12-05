d757c9b0c2d96cc8e844b618d5d7baff202baf61
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-12-05 11:56:09 +0200

    updated to latest build versions and AndroidX, version 1.1.1

--- app/src/main/java/com/romanpulov/libraryandroidtest/PagerActivity.java
+++ app/src/main/java/com/romanpulov/libraryandroidtest/PagerActivity.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v13.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import androidx.legacy.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class PagerActivity extends Activity {
 
